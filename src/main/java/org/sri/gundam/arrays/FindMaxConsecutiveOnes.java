9042fafea32b42460f979ace9c24e2e01604df94
srikanth0917@gmail.com <srikanthgundam@srikanths-mbp.home>
2020-12-06 23:41:49 +0000

    reduced runtime

--- src/main/java/org/sri/gundam/arrays/FindMaxConsecutiveOnes.java
+++ src/main/java/org/sri/gundam/arrays/FindMaxConsecutiveOnes.java
@@ -2,23 +2,20 @@ package org.sri.gundam.arrays;
 
 public class FindMaxConsecutiveOnes {
 
-    int consecutiveCount=0;
-
     public int findMaxConsecutingOnes(int[] nums){
 
         int consecutiveCount = 0;
         int presentCount = 0;
-        for(int i=0; i<nums.length;i++){
+        for(int num:nums){
 
-            if(nums[i] == 1){
+            if(num == 1){
                 presentCount+=1;
             }else{
-                if(consecutiveCount <= presentCount)
-                    consecutiveCount=presentCount;
+                consecutiveCount=Math.max(consecutiveCount,presentCount);
                 presentCount = 0;
             }
         }
-         return consecutiveCount>presentCount?consecutiveCount:presentCount;
+         return Math.max(consecutiveCount,presentCount);
 
 
     }
